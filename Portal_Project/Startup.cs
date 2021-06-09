8a32d1bf31b0fee42ca582e4d17bc002d679b6a2
Reza Kamarian <Kamarian.Reza@GMail.com>
2021-06-09 07:48:03 +0430

    Admin & User Area Added

--- Portal_Project/Startup.cs
+++ Portal_Project/Startup.cs
@@ -84,8 +84,13 @@ namespace Portal_Project
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
+                     name: "areas",
+                     pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+
+                endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
+
                 endpoints.MapRazorPages();
             });
 
